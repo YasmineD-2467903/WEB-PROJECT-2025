1316df7b382f663a46cd2a8253021c724a67f2b9
KaterinaG-2470179 <katerina.ganeva@student.uhasselt.be>
2025-11-12 16:09:32 +0100

    map UI visible (slightly broken) but map grid won't load

--- public/js/group.js
+++ public/js/group.js
@@ -53,6 +53,32 @@
             createList(document.getElementById("viewerList"), viewers);
         }
 
+        if (section === "map") {
+            contentDiv.innerHTML = `
+                <div id="map" style="width: 100%; height: 400px;"></div>
+            `;
+
+            if (!window.googleMapsLoaded) {
+                window.googleMapsLoaded = true;
+
+                const mapsApi = document.createElement("script");
+                mapsApi.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyDf8c_RldjfbjhoyNzxzEYMXt3v8rAVToQ&callback=initMap";
+                mapsApi.async = true;
+                mapsApi.defer = true;
+                document.body.appendChild(mapsApi);
+
+                window.initMap = () => {
+                    const mapOptions = {
+                        center: { lat: 50, lng: 5 },
+                        zoom: 15
+                    };
+                    new google.maps.Map(document.getElementById("map"), mapOptions);
+                };
+            } else {
+                window.initMap();
+            }
+        }
+
     } catch (err) {
         console.error(err);
         contentDiv.innerHTML = `<p class="text-danger">Error loading section.</p>`;
--- public/js/map.js
+++
@@ -1,7 +0,0 @@
-function myMap() {
-const mapOptions = {
-    center: { lat: 50.925093085784084, lng: 5.353779743653817 },
-    zoom: 15,
-};
-new google.maps.Map(document.getElementById("map"), mapOptions);
-}
